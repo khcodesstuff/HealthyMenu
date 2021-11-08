04e6b5a71f66e247ca21fcd8ea5129a3d0fbf38f
khcodesstuff <dmitry.kh4@gmail.com>
2021-11-08 17:57:34 +0200

    Fix

--- javascript/animations.js
+++ javascript/animations.js
@@ -5,21 +5,21 @@ $(function () {
     var block_fruits = document.getElementById('fruits');
 
     window.addEventListener('scroll', () => {
-        anim_based_on_scroll(block_salads, '#salads .design-text-right', 'fadeInRight');
-        anim_based_on_scroll(block_asian, '#asian .design-text-left', 'fadeInLeft');
-        anim_based_on_scroll(block_yogurt, '#yogurt .design-text-left', 'fadeInLeft');
-        anim_based_on_scroll(block_fruits, '#fruits .design-text-right', 'fadeInRight');
+        anim_based_on_scroll(block_salads, '#salads .design-text-right', 'fadeInRight', 200);
+        anim_based_on_scroll(block_asian, '#asian .design-text-left', 'fadeInLeft', 200);
+        anim_based_on_scroll(block_yogurt, '#yogurt .design-text-left', 'fadeInLeft', 200);
+        anim_based_on_scroll(block_fruits, '#fruits .design-text-right', 'fadeInRight', 200);
     });
 });
 
-const anim_based_on_scroll = (block, element_query, animation) => {
+const anim_based_on_scroll = (block, element_query, animation, offset_btm=0) => {
     if (is_scrolled_under(block)){
         animateCSS_reset(element_query, animation);
     }
     else if (is_scrolled_over(block)){
         animateCSS_reset(element_query, animation);
     }
-    else if (is_scrolled_to_bottom(block)){
+    else if (is_scrolled_to_bottom(block, offset_btm)){
         animateCSS(element_query, animation);
     }
 }
@@ -35,11 +35,12 @@ const is_scrolled_over = (element) => {
     element.offsetTop;
 }
 
-const is_scrolled_to_bottom = (element) => {
+const is_scrolled_to_bottom = (element, offset_btm=0) => {
     return  window.scrollY >=
             element.offsetTop +
             element.offsetHeight -
-            window.innerHeight;
+            window.innerHeight -
+            offset_btm;
 }
 
 const animateCSS = (element_query, animation, prefix='animate__') => {
